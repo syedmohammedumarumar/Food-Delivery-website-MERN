21e651e969563423bc01d3b2bac960892ef497eb
Mohammed Umar <140471257+syedmohammedumarumar@users.noreply.github.com>
2025-01-19 23:34:19 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
-  const url="http://localhost:4000";
+  const url="https://food-delivery-website-mern-51xu.onrender.com";
   
   return (
     <div>
